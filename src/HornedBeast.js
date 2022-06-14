1e1ca3c2e5c387e107a51558de8db8540f27fe5d
Matthew Larkin <matthewlarkin8@gmail.com>
2022-06-14 19:33:51 -0400

    Added state to keep track of favorites on each HornedBeast. Created onClick event and handle function so that users can favorite HornedBeast images. Added p tag that displays favorites recevied for each HornedBeast. Imported new css file.

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,10 +1,25 @@
 import React from "react";
+import './HornedBeast.css'
 
 class HornedBeast extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      favorites: 0
+    };
+  };
+
+  handleFavorite=()=>{
+    this.setState({
+      favorites: this.state.favorites+1
+    })
+  }
+
   render(){
     return(
     <>
       <h2>{this.props.title}</h2>
+      <p onClick={this.handleFavorite}>Favorite 💖{this.state.favorites}</p>
       <img src={this.props.imageUrl} alt={this.props.title} title={this.props.title}/>
       <p>{this.props.description}</p>
     </>

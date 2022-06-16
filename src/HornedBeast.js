e2db1530a91e78d35ca304b7ff6a2c6abfc561de
Matthew Larkin <matthewlarkin8@gmail.com>
2022-06-15 22:04:42 -0400

    Created onClick event and handle function for clicking on an image. Passed the URL and description of the beast that was clicked on into the handleOnShow function.

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import Card from 'react-bootstrap/Card'
-import './HornedBeast.css'
+import Card from 'react-bootstrap/Card';
+import './HornedBeast.css';
 
 class HornedBeast extends React.Component{
   constructor(props){
@@ -16,12 +16,15 @@ class HornedBeast extends React.Component{
       favorites: this.state.favorites+1
     })
   }
+  handleImgClick=()=>{
+    this.props.handleOnShow(this.props.imageUrl, this.props.description);
+  }
 
   render(){
     return(
       <Card style={{ width: '20rem' }}>
         <Card.Header as="h2">{this.props.title}</Card.Header>
-        <Card.Img src={this.props.imageUrl} />
+        <Card.Img onClick={this.handleImgClick} src={this.props.imageUrl} alt={this.props.title}/>
         <Card.Body as="card-body">
           <Card.Text as="card-text">{this.props.description}</Card.Text>
           <Button onClick={this.handleFavorite} variant="primary" as="card-button">Favorite 💖{this.state.favorites}</Button>

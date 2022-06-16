359b828cd118f43a6dbaf11db739ca28691db9ab
Matthew Larkin <matthewlarkin8@gmail.com>
2022-06-15 21:59:35 -0400

    Imported data from data.JSON and passed it to Main using props. Imported new SelectedBeast component to display Modals. Created and passed down the necessary functions for SelectedBeast to display different content based on which image is clicked.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
-import React from "react";
-import './reset.css'
+import React from 'react';
+import './reset.css';
 import './App.css';
-import data from './data.json'
-import Header from './Header'
-import Main from "./Main";
-import Footer from "./Footer";
+import data from './data.json';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+import SelectedBeast from './SelectedBeast';
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      showModal: false,
+      selectedBeast: '',
+      beastDesc: ''
+    };
+  };
+
+  handleOnHide=()=>{
+    this.setState({
+      showModal: false
+    });
+  };
+  handleOnShow=(beast,desc)=>{
+    this.setState({
+      showModal: true,
+      selectedBeast: beast,
+      beastDesc: desc
+    });
+  };
+
   render(){
     return (
       <>
         <Header/>
         <Main
         data={data}
+        handleOnShow={this.handleOnShow} 
         />
         <Footer/>
+        <SelectedBeast
+        handleOnHide={this.handleOnHide}
+        showModal={this.state.showModal}
+        selectedBeast={this.state.selectedBeast}
+        beastDesc={this.state.beastDesc}
+        />
       </>  
     );
   }
